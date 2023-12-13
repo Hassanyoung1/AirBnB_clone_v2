6b7b10a03014a53fbf3064057ebc64a57a98bc41
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-13 17:29:23 +1000

    Fabric script that generates a .tgz archive

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from invoke import run as local, task
+import os
+from datetime import datetime
+
+@task
+def do_pack(c):
+    if not os.path.exists("versions"):
+        local("mkdir versions")
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    target_tgz_file = "versions/web_static_{}.tgz".format(timestamp)
+
+    result = local("tar -cvzf {} web_static".format(target_tgz_file))
+
+    if result.ok:
+        return target_tgz_file
+    else:
+        return None
