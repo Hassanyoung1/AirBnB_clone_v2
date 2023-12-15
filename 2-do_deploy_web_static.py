c73fc42f562a7cf6d38fc267a5b473fb43013193
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-15 11:33:47 +1000

    fixed code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,56 @@
 #!/usr/bin/python3
-
+"""Compress web static package
 """
-Fabric script that distributes an archive to your web servers,
-using the function do_deploy
-"""
-
 from fabric.api import *
+from datetime import datetime
 from os import path
 
-""" Define the user, hosts, and key file for Fabric environment """
-env.hosts = ["54.237.1.243", "35.175.132.72"]
+
+env.hosts = ['54.237.1.243', '35.175.132.72']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """Function that distributes an archive to your web servers
-    """
-    """Check if the specified archive_path exists """
-    if not path.exists(archive_path):
-        return False
+        """Deploy web files to server
+        """
+        try:
+                if not (path.exists(archive_path)):
+                        return False
 
-    try:
-        """ Upload the archive to the /tmp/ directory on the server """
-        put(archive_path, "/tmp/")
+                # upload archive
+                put(archive_path, '/tmp/')
 
-        """ Extract information about the archive """
-        archive = archive_path.split("/")[-1]
-        archive_folder = "/data/web_static/releases/" + archive.split(".")[0]
+                # create target dir
+                timestamp = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
 
-        """ Create the necessary directories on the server """
-        run(f"mkdir -p {archive_folder}")
+                # uncompress archive and delete .tgz
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
 
-        """ Extract the contents of the archive to the specified folder """
-        run(f"tar -xzf /tmp/{archive} -C {archive_folder}")
+                # remove archive
+                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
-        """ Remove the uploaded archive from the /tmp/ directory """
-        run(f"rm /tmp/{archive}")
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
-        """ Move the contents of the web_static folder to the parent folder """
-        run(f"mv {archive_folder}/web_static/* {archive_folder}")
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(timestamp))
 
-        """ Remove the now empty web_static folder """
-        run(f"rm -rf {archive_folder}/web_static")
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
 
-        """ Remove the current symbolic link to the web_static folder """
-        run("rm -rf /data/web_static/current")
+                # re-establish symbolic link
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+        except:
+                return False
 
-        """ Create symbolic link to the new vers of the web_static folder """
-        run(f"ln -s {archive_folder} /data/web_static/current")
-        run("sudo service nginx restart")
+        # return True on success
         return True
-    except BaseException:
-        """ Return False if an except occurs during the deployment process """
-        return False
