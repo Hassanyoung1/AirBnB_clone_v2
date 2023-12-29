64917bdc6a8b032f1916e4028833955dfd5e43a2
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-29 21:39:53 +1000

    Display hbnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-
+"""Importing flash module"""
 from flask import Flask
 
-@
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Display hello hbnb"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
